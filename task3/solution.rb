e2318e1f9a4b65a159272fec837f629a2cd75a84
betrakiss <betrakiss@hotmail.com>
2015-11-09 01:18:36 +0200

    Adding BeloteDeck

--- task3/solution.rb
+++ task3/solution.rb
@@ -1,5 +1,7 @@
 
 class Card
+  SUITS = [:diamonds, :spades, :clubs, :hearts].freeze
+
   attr_reader :rank
   attr_reader :suit
 
@@ -76,13 +78,19 @@ class Deck
   end
 end
 
-class WarHand
+class Hand
   attr_reader :cards
 
   def initialize(cards)
     @cards = cards
   end
 
+  def size()
+    @cards.size
+  end
+end
+
+class WarHand < Hand
   def play_card()
     @cards.shift
   end
@@ -94,11 +102,88 @@ end
 
 class WarDeck < Deck
   HAND_SIZE = 26
+  TOTAL_CARDS = 52
+
+  def Mooo_sodas_ASDA()
+  end
 
   def deal()
     hand = []
     HAND_SIZE.times { hand << draw_top_card }
     WarHand.new(hand)
+  end
+end
+
+class BeloteHand < Hand
+  POWER = Hash[[7, 8, 9, :jack, :queen, :king, 10, :ace].map.with_index.to_a]
+
+  def highest_of_suit(suit)
+    highest = 0
+    @cards.each { |card| highest = POWER[card] if card.rank > highest }
+
+    highest
+  end
+
+  def belote?()
+    kings = @cards.select { |card| card.rank == :king }
+    kings.each do |king|
+      return true if kings.select do |card|
+        card.rank == :queen and card.suit == king.suit
+      end
+    end
+
+    false
+  end
+
+  def n_in_a_row?(amount)
+    @cards.sort! { |a, b| POWER[a.rank] <=> POWER[b.rank] }
+    previous = @cards.first
+
+    @cards.each do |current|
+      matches = (current.suit == previous.suit and
+                 current > previous) ? matches += 1 : 0
+
+      return true if matches == amount
+      previous = current
+    end
+
+    false
+  end
+
+  def tierce?()
+    n_in_a_row(3)
+  end
+
+  def quarte?()
+    n_in_a_row(4)
+  end
+
+  def quint?()
+    n_in_a_row(5)
+  end
+
+  def carre_of_x?(rank)
+    @cards.select { |card| card.rank == rank } .size == 4
+  end
+
+  def carre_of_jacks?()
+    carre_of_x?(:jack)
+  end
+
+  def carre_of_jacks?()
+    carre_of_x?(9)
+  end
+
+  def carre_of_jacks?()
+    carre_of_x?(:ace)
+  end
+end
+
+class BeloteDeck < Deck
+  HAND_SIZE = 8
+  TOTAL_CARDS = 32
+
+  def deal()
   end
 end
 
@@ -108,6 +193,6 @@ end
 #          Card.new(4, :carre)]
 
 
-deck = WarDeck.new();
+deck = WarDeck.new()
 deck.shuffle()
 puts deck.deal.cards.join ', '
